b5c67d778d1a515161c7ecd3fe4c903a7aece7da
Kevin McQuown <kevinmcquown@mac.com>
2022-11-26 16:45:23 -0600

    updates

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
-sudo apt-get --assume-yes update
 sudo apt --assume-yes install zsh
 sudo chsh -s $(which zsh) $USER
 exec zsh
-sudo apt --assume-yes install git
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+sudo apt --assume-yes install stow
+cd ~/.dotfiles
+stow git
+stow zsh
+stow p10k
+
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
