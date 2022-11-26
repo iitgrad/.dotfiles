9c64f02e7880d7293968aa03fc311e55271ad17e
Kevin McQuown <kevinmcquown@me.com>
2022-11-26 13:33:32 -0600

    more changes

--- nvim/.config/nvim/lua/iitgrad/plugins-setup.lua
+++ nvim/.config/nvim/lua/iitgrad/plugins-setup.lua
@@ -64,7 +64,11 @@ return packer.startup(function(use)
 
 	-- fuzzy finding w/ telescope
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
 	-- autocompletion
 	use("hrsh7th/nvim-cmp") -- completion plugin
