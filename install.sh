6eacd4178c519e1cbc00d5c2e3ab0f152b3471d6
Kevin McQuown <kevinmcquown@mac.com>
2022-11-26 14:52:34 -0600

    updates

--- install.sh
+++ install.sh
@@ -6,14 +6,15 @@ nix-env -iA nixpkgs.zsh \
   nixpkgs.tmux \
   nixpkgs.stow \
   nixpkgs.bat \
-  nixpkgs.direnv \
-  nixpdgs.oh-my-zsh
+  nixpkgs.direnv
 
 # add zsh into legal shells
 command -v zsh | sudo tee -a /etc/shells
 
 # change to the zsh shell
 sudo chsh -s $(which zsh) $USER
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # bundle zsh plugins
 antibody bundle < ~/.zsh_plugins.txt > ~/.zsh.plugins.sh
