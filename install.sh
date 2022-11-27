8124884e1371603a12cef00b1df53c9dd95e306e
Kevin McQuown <kevinmcquown@mac.com>
2022-11-27 07:53:25 -0600

    updates

--- install.sh
+++ install.sh
@@ -4,18 +4,3 @@
 sudo apt --assume-yes install stow
 sudo apt --assume-yes install zsh
 source ./step2.sh
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-echo 'source ~/.p10k.zsh' >>~/.zshrc
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-cd ~/.dotfiles
-stow git
-stow zsh
-stow p10k
-
-# sudo apt --assume-yes install neovim
-# sudo apt --assume-yes install nodejs
-# sudo apt --assume-yes install npm
