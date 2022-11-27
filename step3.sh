feb17ae6917ebcf12ed0042ea15df2391290142a
Kevin McQuown <kevinmcquown@mac.com>
2022-11-27 10:48:43 -0600

    updates

--- step3.sh
+++ step3.sh
@@ -3,6 +3,7 @@ echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 echo 'source ~/.p10k.zsh' >>~/.zshrc
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 cd ~/.dotfiles
 stow git
 stow p10k
