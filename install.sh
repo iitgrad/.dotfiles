e739f0983713deb5d836fb00ad2222f532fbec0a
Kevin McQuown <kevinmcquown@mac.com>
2022-11-27 07:17:18 -0600

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-sudo apt --assume-yes install git
+# These two commands should be done before running this script
+# sudo apt-get update
+# sudo apt --assume-yes install git
 sudo apt --assume-yes install stow
 sudo apt --assume-yes install zsh
 sudo chsh -s $(which zsh) $USER
